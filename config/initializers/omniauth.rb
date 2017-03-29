fbcd3123349ad2f88db5d820de05381d735a6caf
puan0601 <anton.pugach@gmail.com>
2017-03-29 10:12:04 -0700

    github login force redirect_uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret, scope: "user:email, user:name"
+  provider :github, Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret, scope: "user:email", redirect_uri: '/auth/github/callback' 
 end
